7d9c3b7b0607df8d8b0b99ddbcf6e417af0bc6d1
Ihor Novikov <novi0003@hz.nl>
2024-04-13 16:52:35 +0200

    commented out unnecessary line

--- assets/app.js
+++ assets/app.js
@@ -7,4 +7,4 @@ import './bootstrap.js';
  */
 import './styles/app.css';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+// console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
