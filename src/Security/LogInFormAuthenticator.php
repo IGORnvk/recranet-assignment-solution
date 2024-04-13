b5ecf0af5988e7624ea6fbc2a667aa8e90d0597a
Ihor Novikov <novi0003@hz.nl>
2024-04-13 20:28:31 +0200

    fixed the authenticator

--- src/Security/LogInFormAuthenticator.php
+++ src/Security/LogInFormAuthenticator.php
@@ -8,19 +8,19 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\UserNotFoundException;
-use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
-use Symfony\Component\Security\Http\SecurityRequestAttributes;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
-class LogInFormAuthenticator extends AbstractAuthenticator implements AuthenticationEntryPointInterface
+class LogInFormAuthenticator extends AbstractLoginFormAuthenticator
 {
+    use TargetPathTrait;
+
     private UserRepository $userRepository;
 
     private RouterInterface $router;
@@ -31,15 +31,10 @@ class LogInFormAuthenticator extends AbstractAuthenticator implements Authentica
         $this->router = $router;
     }
 
-    public function supports(Request $request): ?bool
-    {
-        return ($request->getPathInfo() == '/login' && $request->isMethod('POST'));
-    }
-
     public function authenticate(Request $request): Passport
     {
-        $email = $request->request->get('email');
-        $password = $request->request->get('password');
+        $email = $request->request->get('log_in_form[email]');
+        $password = $request->request->get('log_in_form[password]');
 
         return new Passport(
             new UserBadge($email, function($userIdentifier) {
@@ -65,24 +60,19 @@ class LogInFormAuthenticator extends AbstractAuthenticator implements Authentica
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
+        // redirect to previous url, if any
+        if ($target == $this->getTargetPath($request->getSession(), $firewallName)) {
+            return new RedirectResponse($target);
+        }
+
+        // otherwise redirect to home
         return new RedirectResponse(
           $this->router->generate('app_home')
         );
     }
 
-    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    protected function getLoginUrl(Request $request): string
     {
-        $request->getSession()->set(SecurityRequestAttributes::AUTHENTICATION_ERROR, $exception);
-
-        return new RedirectResponse(
-          $this->router->generate('app_login')
-        );
-    }
-
-    public function start(Request $request, AuthenticationException $authException = null): Response
-    {
-        return new RedirectResponse(
-            $this->router->generate('app_login')
-        );
+        return $this->router->generate('app_login');
     }
 }
