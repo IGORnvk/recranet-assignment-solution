76c9ce873c97a675f4789f9d20ebf355660a85c7
Ihor Novikov <novi0003@hz.nl>
2024-04-13 18:38:50 +0200

    created a skeleton for future command

---
+++ src/Command/FootballInfoCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'FootballInfoCommand',
+    description: 'Retrieves information about Eredivisie matches',
+)]
+class FootballInfoCommand extends Command
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+          ->setDescription('Retrieves information about Eredivisie matches')
+          ->setHelp('This command allows you to get data about Eredivisie matches and their results')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $arg1 = $input->getArgument('arg1');
+
+        if ($arg1) {
+            $io->note(sprintf('You passed an argument: %s', $arg1));
+        }
+
+        if ($input->getOption('option1')) {
+            // ...
+        }
+
+        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+
+        return Command::SUCCESS;
+    }
+}
